1edfca7fb0a89281eb3fb3340777904aa91caeda
Vijay <vijaythevirtue@gmail.com>
2021-01-02 17:17:03 +0530

    first commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Parameters"),
+      ),
+      body: buildTextWidget(
+        "Flutter", //Positional Parameters
+        myFontSize: 25.30, //Named Parameters
+      ),
+    );
+  }
+
+  var buildTextWidget = (String myText, {double myFontSize}) => Center(
+        child: Text(
+          myText,
+          style: TextStyle(color: Colors.blue, fontSize: myFontSize),
+        ),
+      );
+}
